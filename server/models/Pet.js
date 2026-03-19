60e31b1a83140a72f9832c11087f7f7f64b00089
Ben Spector <111444562+benspector-mls@users.noreply.github.com>
2026-03-19 09:59:34 -0400

    Refactor Pet class methods to module exports

--- server/models/Pet.js
+++ server/models/Pet.js
@@ -6,33 +6,30 @@ const getId = () => id++;
 // Each pet should have an id (from getId) and a name.
 
 
-class Pet {
-  // TODO: Create a new pet and add it to the array. Return the new pet.
-  static create(name) {
 
-  }
+// TODO: Create a new pet and add it to the array. Return the new pet.
+module.exports.create = (name) => {
 
-  // TODO: Return a copy of all pets.
-  static list() {
-
-  }
-
-  // TODO: Find and return a single pet by its ID.
-  static find(id) {
-
-  }
-
-  // TODO: Update the name of a pet. Return the updated pet,
-  // or null if not found.
-  static update(id, newName) {
-
-  }
-
-  // TODO: Remove a pet from the array. Return the deleted pet,
-  // or null if not found.
-  static delete(id) {
-
-  }
 }
 
-module.exports = Pet;
+// TODO: Return a copy of all pets.
+module.exports.list = () => {
+
+}
+
+// TODO: Find and return a single pet by its ID.
+module.exports.find = (id) => {
+
+}
+
+// TODO: Update the name of a pet. Return the updated pet,
+// or null if not found.
+module.exports.update = (id, newName) => {
+
+}
+
+// TODO: Remove a pet from the array. Return the deleted pet,
+// or null if not found.
+module.exports.destroy = (id) => {
+
+}
